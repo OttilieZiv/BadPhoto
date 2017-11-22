bf6520a5cbbb613c65807589d0c4c0997a06effe
Ottilie Zivanovic <me@ottilie.co.uk>
2017-11-22 11:51:23 +0000

    First models defined, migrated

--- app/models.py
+++ app/models.py
@@ -1,7 +1,35 @@
-"""
-Definition of models.
-"""
-
+import uuid
 from django.db import models
+from imagekit.models import ProcessedImageField
+from imagekit.processors import ResizeToFit
 
-# Create your models here.
+class Gallery(models.Model):
+    title = models.CharField(max_length=70)
+    description = models.TextField(max_length=1024)
+    thumb = ProcessedImageField(upload_to='galleries', processors=[ResizeToFit(300)], format='JPEG', options={'quality': 90})
+    tags = models.CharField(max_length=250)
+    is_visible = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now_add=True)
+    slug = models.SlugField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        db_table = 'badphoto_gallery'
+        verbose_name_plural = 'Galleries'
+
+class GalleryImage(models.Model):
+    image = ProcessedImageField(upload_to='galleries', processors=[ResizeToFit(1280)], format='JPEG', options={'quality': 70})
+    thumb = ProcessedImageField(upload_to='galleries', processors=[ResizeToFit(300)], format='JPEG', options={'quality': 80})
+    gallery = models.ForeignKey(Gallery)
+    alt = models.CharField(max_length=255, default=uuid.uuid4)
+    created = models.DateTimeField(auto_now_add=True)
+    width = models.IntegerField(default=0)
+    height = models.IntegerField(default=0)
+    slug = models.SlugField(max_length=70, default=uuid.uuid4, editable=False)
+
+    class Meta:
+        db_table = 'badphoto_image'
+
